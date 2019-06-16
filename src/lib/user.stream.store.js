7604c72a297d3c564f517a9f0c958c533630d23e
Andy Spaven <andy.spaven@digital.hmrc.gov.uk>
2019-06-16 18:33:57 +0100

    Add user stream store as a fake db

---
+++ src/lib/user.stream.store.js
@@ -0,0 +1,41 @@
+const db = {};
+
+function exists(name) {
+  return Object.keys(db).includes(name);
+}
+export const purge = () => {
+  Object.keys(db).forEach((key) => {
+    delete db[key];
+  });
+};
+
+export const findUser = (name) => {
+  if (exists(name)) {
+    return db[name];
+  }
+  return null;
+};
+
+export const addStreamToUser = ({ name, id }) => {
+  if (exists(name)) {
+    db[name].push(id);
+  } else {
+    db[name] = [id];
+  }
+};
+
+export const removeStreamFromUser = ({ name, id }) => {
+  if (exists(name) && db[name].includes(id)) {
+    const existing = db[name];
+
+    if (existing.length === 1) {
+      delete db[name];
+    } else {
+      console.log(`before: ${existing}`);
+      const idx = existing.findIndex(i => i === id);
+      existing.splice(idx, 1);
+      db[name] = existing
+      console.log(`after: ${db[name]}`);
+    }
+  }
+};
---
+++ tests/lib/user.stream.store.test.js
@@ -0,0 +1,70 @@
+import chai from 'chai';
+import { describe, it } from 'mocha';
+import {
+  purge,
+  findUser,
+  addStreamToUser,
+  removeStreamFromUser,
+} from '../../src/lib/user.stream.store';
+
+const { expect } = chai;
+
+chai.should();
+
+const userShouldNotExist = name => expect(findUser(name)).to.be.null;
+const userShouldExist = name => expect(findUser(name)).not.to.be.null;
+
+describe('User Stream Store', () => {
+  describe('purge', () => {
+    it('should reset the database', () => {
+      purge();
+      userShouldNotExist('alice');
+      userShouldNotExist('bob');
+    });
+  });
+
+  describe('findUser', () => {
+    it('should return an empty array for a non existant user', () => {
+      userShouldNotExist('bob');
+    });
+  });
+
+  describe('addStreamToUser', () => {
+    it('should create a user record if none exist and include the id', () => {
+      purge();
+      addStreamToUser({ name: 'alice', id: '123' });
+      findUser('alice').should.eql(['123']);
+      userShouldNotExist('bob');
+    });
+    it('should add streams to an existing user record', () => {
+      purge();
+      addStreamToUser({ name: 'alice', id: '123' });
+      addStreamToUser({ name: 'alice', id: '234' });
+      findUser('alice').should.eql(['123', '234']);
+    });
+  });
+
+  describe('removeStreamFromUser', () => {
+    it('should remove an id from the given user only', () => {
+      purge();
+      addStreamToUser({ name: 'alice', id: '123' });
+      addStreamToUser({ name: 'alice', id: '234' });
+      addStreamToUser({ name: 'alice', id: '345' });
+      addStreamToUser({ name: 'bob', id: '123' });
+
+      removeStreamFromUser({ name: 'alice', id: '123' });
+
+      findUser('alice').should.eql(['234', '345']);
+      userShouldExist('bob');
+    });
+
+    it('should remove the user from the db if the id removed is the last one', () => {
+      purge();
+      addStreamToUser({ name: 'alice', id: '123' });
+      addStreamToUser({ name: 'bob', id: '123' });
+      removeStreamFromUser({ name: 'alice', id: '123' });
+      userShouldNotExist('alice');
+      userShouldExist('bob');
+    });
+  });
+});
